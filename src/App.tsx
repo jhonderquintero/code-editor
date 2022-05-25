17841adbf61c9bc1fad61658ce0f4b28d28381ae
jhonderquintero <jhonderquintero@gmail.com>
2022-05-25 11:34:31 -0400

    CodeEditor: initial basic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { CodeEditor } from './components/CodeEditor';
+import './styles/normalize.css';
+
 export const App = () => {
-  return <div>React w/ Webpack and Typescript Template </div>;
+  return (
+    <div style={{ width: '100%', height: 80 }}>
+      <CodeEditor identifier="first" />
+      <CodeEditor identifier="second" />
+    </div>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,2 @@
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const container = document.getElementById('root')!;
-const root = createRoot(container);
-
-root.render(<App />);
+export { CodeEditor } from './components/CodeEditor';
